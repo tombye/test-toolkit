0c62005ff227db9fb54d1ff0534641426aecdf0b
Tom Byers <tombaromba@gmail.com>
2014-12-30 14:07:42 +0000

    Move Sass options out of method

--- build_tools/asset_compiler.py
+++ build_tools/asset_compiler.py
@@ -6,11 +6,17 @@ class AssetCompiler(object):
     self.repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     self.sass_src_root = os.path.join(self.repo_root, "sass")
     self.sass_dest_root = os.path.join(self.repo_root, "stylesheets")
+    self.default_options = {
+      "output_style" : "nested",
+      "include_paths" : [os.path.join(self.repo_root, "govuk_frontend_toolkit/stylesheets/")]
+    }
 
   def compile_file(self, src_path_abs):
+    sass_options = self.default_options
+    sass_options["filename"] = src_path_abs
     dest_path_abs = src_path_abs.replace(self.sass_src_root, self.sass_dest_root)
     dest_path_abs = self.__change_extension_to(dest_path_abs, "css")
-    result = sass.compile(filename=src_path_abs, output_style="nested")
+    result = sass.compile(**sass_options)
     open(dest_path_abs, "w+").write(result)
 
   def compile(self):
